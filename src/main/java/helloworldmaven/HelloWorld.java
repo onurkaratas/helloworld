1d8a696835d28df4601535f9433922be090cc314
onkar.atasi@gmail.com <onkar.atasi@gmail.com>
2015-01-27 08:11:37 +0200

    3. commit java calısma

--- src/main/java/helloworldmaven/HelloWorld.java
+++ src/main/java/helloworldmaven/HelloWorld.java
@@ -7,14 +7,16 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JOptionPane.showConfirmDialog(null, "programýn çalýþmasýný istiyor musunuz");
+		int secim = JOptionPane.showConfirmDialog(null, "Syý tahmin oyunun çalýþmasýný istiyor musunuz");
+		if (secim == JOptionPane.YES_OPTION)
+		{
+			
 		int tahmn_sayisi = 50;
 
-		JOptionPane.showMessageDialog(null,"Merhaba Dünya");
+		
 		String sayi = JOptionPane.showInputDialog("bir sayý giriniz");
 		int sayi1 = Integer.parseInt(sayi);
-		JOptionPane.showMessageDialog(null, "girdiðiniz sayý =  "  +sayi1);
-	do{
+		do{
 		if(tahmn_sayisi<sayi1)
 		{
 			JOptionPane.showMessageDialog(null, "girdiðiniz sayýyý küçültün");
@@ -23,11 +25,15 @@ public class HelloWorld {
 			JOptionPane.showMessageDialog(null, "girdiðiniz sayýyý büyültün ");
 			}
 		sayi = JOptionPane.showInputDialog("bir sayý giriniz");
-		sayi1 = Integer.parseInt(sayi);
-	}while (tahmn_sayisi != sayi1);
-	JOptionPane.showMessageDialog(null, "doðru tahmin" +tahmn_sayisi);
-		}
+		sayi1  = Integer.parseInt(sayi);
 	
-	}
+		}
+		while (tahmn_sayisi != sayi1);
+	JOptionPane.showMessageDialog(null, "doðru tahmin =" +tahmn_sayisi);
+		}
+		}
+}
+	
+	
 
 
