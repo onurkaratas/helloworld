d57bb646b6b7c4fe751bd6537e360568a6a117f1
onkar.atasi@gmail.com <onkar.atasi@gmail.com>
2015-01-27 07:22:47 +0200

    2. commit ve eclipse çalışma

--- src/main/java/helloworldmaven/HelloWorld.java
+++ src/main/java/helloworldmaven/HelloWorld.java
@@ -7,8 +7,27 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		JOptionPane.showConfirmDialog(null, "programýn çalýþmasýný istiyor musunuz");
+		int tahmn_sayisi = 50;
 
 		JOptionPane.showMessageDialog(null,"Merhaba Dünya");
+		String sayi = JOptionPane.showInputDialog("bir sayý giriniz");
+		int sayi1 = Integer.parseInt(sayi);
+		JOptionPane.showMessageDialog(null, "girdiðiniz sayý =  "  +sayi1);
+	do{
+		if(tahmn_sayisi<sayi1)
+		{
+			JOptionPane.showMessageDialog(null, "girdiðiniz sayýyý küçültün");
+		}
+			else{ 
+			JOptionPane.showMessageDialog(null, "girdiðiniz sayýyý büyültün ");
+			}
+		sayi = JOptionPane.showInputDialog("bir sayý giriniz");
+		sayi1 = Integer.parseInt(sayi);
+	}while (tahmn_sayisi != sayi1);
+	JOptionPane.showMessageDialog(null, "doðru tahmin" +tahmn_sayisi);
+		}
+	
 	}
 
-}
+
